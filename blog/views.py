a46a173e629b76ec39f7ad0d850b08bcae976572
May Al Sanea <developer.web.dj@gmail.com>
2024-01-19 04:24:39 +0300

    fix errors in template index.html design

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category,Tag
 
 
 def posts_view(request):
     categories = Category.objects.all()
+    tags = Tag.objects.all()
     posts = Post.objects.all()
+    
     context ={
         'categories' : categories,
-        'posts' : posts,    
+        'tags'       : tags,
+        'posts'      : posts,    
     }
     return render(request,'blog/index.html', context)
-
-
