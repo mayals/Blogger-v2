dc614e9527a765de4af5fde1cdf46488aa2f5c6c
May Al Sanea <developer.web.dj@gmail.com>
2024-01-21 19:31:32 +0300

    modify blog.views.post_delete_confirm

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('new-post/', views.post_create, name='new-post'),
     path('post-detail/<slug:slug>/', views.post_detail, name='post-detail'),
-    path('post-update/<slug:slug>/', views.post_update, name= 'post-update')
+    path('post-update/<slug:slug>/', views.post_update, name= 'post-update'),
+    path('post-delete-confirm/<slug>/', views.post_delete_confirm, name ='post-delete-confirm'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Post, Category,Tag
 from .forms import PostForm
 
@@ -17,7 +18,7 @@ def home_view(request):
     return render(request,'blog/home.html', context)
 
 
-
+@login_required()
 def post_create(request):
     form = PostForm()
     if request.method == 'POST' :
@@ -59,27 +60,55 @@ def post_detail(request,slug):
 
 
 
-def post_update(request,slug):
+
+@login_required()
+def post_update(request,slug): 
     post = get_object_or_404(Post, slug=slug)
-    form= PostForm(instance=post)
-    print(post)
-    if request.method == 'POST':
-        form = PostForm(request.POST,request.FILES,instance=post)
-        print(form) 
-        if form.is_valid():
-            updated_post = form.save(commit = False)
-            updated_post.author = request.user
-            updated_post.save()
+    if post.author == request.user :
+        form= PostForm(instance=post)
+        if request.method == 'POST':
+            form = PostForm(request.POST,request.FILES,instance=post)
             print(form) 
-            messages.success(request,f'Thanks ( {request.user.first_name} ), your post updated successfully !')
-            form = PostForm()
-            return redirect('blog:home')
-        
-        else:
-           form = PostForm(request.POST,request.FILES,instance=post)
-           messages.error(request,f'Post not update correctly! please complete those fields below.!')
+            if form.is_valid():
+                updated_post = form.save(commit = False)
+                updated_post.author = request.user
+                updated_post.save()
+                form.save_m2m() 
+                messages.success(request,f'Thanks ( {request.user.first_name} ), your post updated successfully !')
+                return redirect('blog:home')
+            
+            else:
+                form = PostForm(request.POST,request.FILES,instance=post)
+                messages.error(request,f'Post not update correctly! please complete those fields below.!')
              
+    else:
+        messages.warning(request,f"Sorry, you have no permission to update this post, only post's author can update it")
+        return redirect('blog:home')
+    
     context ={
         'form' : form ,
     }
     return render(request,'blog/post_update.html',context)
+
+
+
+
+@login_required()
+def post_delete_confirm(request,slug):
+    post = get_object_or_404(Post, slug=slug)
+    if post.author == request.user :
+        if request.method == 'POST' and 'yes-delete'in request.POST:
+            post.delete()
+            messages.success(request,f'Thanks ( {request.user.first_name} ), your post deleted successfully !')
+            return redirect('blog:home')
+                
+        context ={
+            'post' : post ,
+        }
+        return render(request,'blog/post_delete_confirm.html',context)
+
+        
+    else:
+        messages.warning(request,f"Sorry, you have no permission to delete this post, only post's author can delete it")
+        return redirect('blog:home')
+    
