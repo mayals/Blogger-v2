c3e7710354d36b8f1e2ff3b5f570f0e99ad88117
May Al Sanea <developer.web.dj@gmail.com>
2024-02-26 01:16:18 +0300

    modify robots.txt file and path to it in urls.py

--- blogger/urls.py
+++ blogger/urls.py
@@ -7,7 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.sitemaps import GenericSitemap  # new
 from django.contrib.sitemaps.views import sitemap  # new
 from blog.sitemap import StaticSitemap, PostSitemap, CategorySitemap, TagSitemap 
-
+from django.views.generic import TemplateView
 
 
 sitemaps = {
@@ -19,6 +19,9 @@ sitemaps = {
 
 
 urlpatterns = [
+   # robots.txt
+   path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+   
    path('admin/', admin.site.urls),
    path('user/', include('user.urls', namespace='user')),
    path('', include('blog.urls', namespace='blog')),
